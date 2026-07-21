da885f8c8fab2185eb64cbefb33536427dc15433
farid <deltaburs@gmail.com>
2026-07-21 12:13:25 +0330

    main function

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	"github.com/deblinux123/go-fiber/database"
+	"github.com/deblinux123/go-fiber/handler"
+	"github.com/gofiber/fiber/v3"
+)
+
+type User struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func main() {
+	// connecting to database
+	database.Connect()
+
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("hello Fiber!")
+	})
+
+	// add new router
+	app.Get("/about", func(c fiber.Ctx) error {
+		return c.SendString("This is about page and nothing else.")
+	})
+
+	app.Get("/wellcome", func(c fiber.Ctx) error {
+		return c.SendString("Welcome to my page")
+	})
+
+	app.Get("/contact", func(c fiber.Ctx) error {
+		return c.SendString("Contact us")
+	})
+
+	// return json file
+	app.Get("/user", handler.GetUsers)
+
+	// post request
+	app.Post("/signup", handler.SignUp)
+
+	log.Fatal(app.Listen(":3000"))
+}
